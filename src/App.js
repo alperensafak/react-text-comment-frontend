2de25b4fcea0d6e13cc11de2fb9e08d28d650d93
Alperen <alperensafak@gmail.com>
2021-04-10 20:24:25 +0300

    got datas with axios, created text lists and route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-
+import React from "react"
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+import Posts from "./components/Posts"
+import PostContent from "./components/PostContent";
 
 function App() {
-  return (
 
-      <div className={"main-wrapper"}>
-          <header>
+    return (
+        <Router>
+            <div className={"main-wrapper"}>
+                <header>
+                    <div className="ui three item menu">
+                        <Link  to="/" className="item active">HOME</Link>
+
+                    </div>
+                </header>
+
+                <div className="ui raised very padded text container segment">
+
+                    <Route path="/" exact component={Posts}/>
+                    <Route path="/posts/:id" component={PostContent}/>
 
 
-          </header>
+                </div>
 
-          <section className={"post-list"}>
-
-          </section>
-
-
-      </div>
-
-  );
+            </div>
+        </Router>
+    );
 }
 
 export default App;
