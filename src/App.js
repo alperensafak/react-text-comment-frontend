0ba372efea1f46a14402a432584cb75dfe6c74f5
Alperen <alperensafak@gmail.com>
2021-04-13 23:16:43 +0300

    add text, edit text, comment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from "react"
 import {BrowserRouter as Router, Link, Route} from "react-router-dom";
-import Posts from "./components/Posts"
-import PostContent from "./components/PostContent";
+import Texts from "./components/Texts"
+import TextContent from "./components/TextContent";
+import AddText from "./components/AddText";
+import EditText from "./components/EditText";
 
 function App() {
+
+
 
     return (
         <Router>
             <div className={"main-wrapper"}>
                 <header>
-                    <div className="ui three item menu">
-                        <Link  to="/" className="item active">HOME</Link>
 
-                    </div>
+                    <div className="ui secondary pointing menu">
+                        <a href="/" className="item active">
+                            Home
+                        </a>
+                        <a href="/addtext" className="item">
+                            addtext
+                        </a>
+                        </div>
                 </header>
 
                 <div className="ui raised very padded text container segment">
 
-                    <Route path="/" exact component={Posts}/>
-                    <Route path="/posts/:id" component={PostContent}/>
+                    <Route path="/" exact component={Texts}/>
+                    <Route path="/posts/:id" exact component={TextContent}/>
+                    <Route path="/addtext" component={AddText}/>
+                    <Route path="/posts/:id/edit" component={EditText}/>
 
 
                 </div>
